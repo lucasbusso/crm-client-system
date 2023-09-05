46b50d6b44b93f6892baac0f78e4958d14470a98
Lucas Busso <lucasxbusso@gmail.com>
2023-09-04 21:10:43 -0300

    single client component

---
+++ src/components/Client.tsx
@@ -0,0 +1,26 @@
+const Client = () => {
+  return (
+    <div className="m-3 bg-white shadow-md rounded-md px-5 py-10 ">
+      <p className="uppercase font-bold text-slate-600 my-3">
+        Name: <span className="font-normal normal-case">loquito</span>
+      </p>
+
+      <p className="uppercase font-bold text-slate-600 my-3">
+        Business name: <span className="font-normal normal-case">loquito</span>
+      </p>
+
+      <p className="uppercase font-bold text-slate-600 my-3">
+        Client since: <span className="font-normal normal-case">fecha</span>
+      </p>
+
+      <p className="uppercase font-bold text-slate-600 my-3">
+        Description:{" "}
+        <span className="font-normal normal-case">
+          loquitoloquitoloquitoloquito loquito
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Client;
