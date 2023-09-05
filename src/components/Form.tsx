1c5704bd6dd35dec134b4178ec7942b71209cb5d
Lucas Busso <lucasxbusso@gmail.com>
2023-09-04 22:58:04 -0300

    form states

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,27 @@
+import React from "react";
+import { useForm } from "../hooks";
+
 const Form = () => {
+  const { formData, handleInputChange } = useForm({
+    name: "",
+    business: "",
+    email: "",
+    date: "",
+    description: "",
+  });
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("Submit", formData);
+  }
+
   return (
-    <div className="md:w-1/2 w-full">
+    <div className="md:w-1/2 w-full mx-5">
       <h2 className="font-bold text-2xl my-6 text-center">Add new client</h2>
-      <form className="bg-white shadow-sm rounded-md py-10 px-5">
+      <form
+        className="bg-white shadow-sm rounded-md py-10 px-5"
+        onSubmit={(e) => handleSubmit(e)}
+      >
         <div className="block mb-6">
           <label htmlFor="name" className="uppercase font-bold text-slate-600">
             Name
@@ -11,6 +30,8 @@ const Form = () => {
             name="name"
             type="text"
             className="border-2 w-full p-2 mt-2 placeholder-slate-500 rounded-md"
+            value={formData.name}
+            onChange={handleInputChange}
           />
         </div>
         <div className="block mb-6">
@@ -24,6 +45,20 @@ const Form = () => {
             name="business"
             type="text"
             className="border-2 w-full p-2 mt-2 placeholder-slate-500 rounded-md"
+            value={formData.business}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="block mb-6">
+          <label htmlFor="email" className="uppercase font-bold text-slate-600">
+            Email
+          </label>
+          <input
+            name="email"
+            type="email"
+            className="border-2 w-full p-2 mt-2 placeholder-slate-500 rounded-md"
+            value={formData.email}
+            onChange={handleInputChange}
           />
         </div>
         <div className="block mb-6">
@@ -34,6 +69,8 @@ const Form = () => {
             name="date"
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-slate-500 rounded-md"
+            value={formData.date}
+            onChange={handleInputChange}
           />
         </div>
         <div className="block mb-6">
@@ -41,8 +78,11 @@ const Form = () => {
             Description (optional)
           </label>
           <textarea
+            name="description"
             id="description"
             className="border-2 w-full p-2 mt-2 placeholder-slate-500 rounded-md"
+            value={formData.description}
+            onChange={handleInputChange}
           />
         </div>
 
