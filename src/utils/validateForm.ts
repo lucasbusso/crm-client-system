ebc469f234d53e8ee75f341e31f43b893a7b1364
Lucas Busso <lucasxbusso@gmail.com>
2023-09-05 13:47:46 -0300

    form validation with yup use case

---
+++ src/utils/validateForm.ts
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+export const FormValidator = yup.object().shape({
+  name: yup
+    .string()
+    .trim()
+    .required("Client name is empty")
+    .min(4, "The name is too short"),
+  business: yup.string().trim().required("Business name is empty"),
+  email: yup
+    .string()
+    .email()
+    .required("Email is empty")
+    .min(8, "The email is too short"),
+  date: yup.string().trim().required("Date is empty"),
+  description: yup.string().trim(),
+});
