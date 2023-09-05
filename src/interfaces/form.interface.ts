4f155612920635d78db7a2eb601d762f18d28b96
Lucas Busso <lucasxbusso@gmail.com>
2023-09-04 22:58:23 -0300

    register form interface

---
+++ src/interfaces/form.interface.ts
@@ -0,0 +1,17 @@
+import { ChangeEvent } from "react";
+
+export interface FormValues {
+  name: string;
+  business: string;
+  date: string;
+  email: string;
+  description: string;
+}
+
+export interface FormHooks {
+  formData: FormValues;
+  handleInputChange: (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  resetForm: () => void;
+}
