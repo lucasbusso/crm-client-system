307e5feb7cee7168e4d5767e93b6fc833ef90522
Lucas Busso <lucasxbusso@gmail.com>
2023-09-07 18:32:59 -0300

    client component real data mapping

--- src/components/Client.tsx
+++ src/components/Client.tsx
@@ -1,25 +1,44 @@
+import { useClientContext } from "../context/client.context";
+
 const Client = () => {
+  const { clients } = useClientContext();
+
   return (
-    <div className="m-3 bg-white shadow-md rounded-md px-5 py-10 ">
-      <p className="uppercase font-bold text-slate-600 my-3">
-        Name: <span className="font-normal normal-case">loquito</span>
-      </p>
+    <>
+      {clients.length > 0 &&
+        clients.map((client) => (
+          <div
+            className="m-3 bg-white shadow-md rounded-md px-5 py-10 "
+            key={client.name}
+          >
+            <p className="uppercase font-bold text-slate-600 my-3">
+              Name:{" "}
+              <span className="font-normal normal-case">{client.name}</span>
+            </p>
+            <p className="uppercase font-bold text-slate-600 my-3">
+              Client email:{" "}
+              <span className="font-normal normal-case">{client.email}</span>
+            </p>
 
-      <p className="uppercase font-bold text-slate-600 my-3">
-        Business name: <span className="font-normal normal-case">loquito</span>
-      </p>
+            <p className="uppercase font-bold text-slate-600 my-3">
+              Business name:{" "}
+              <span className="font-normal normal-case">{client.business}</span>
+            </p>
 
-      <p className="uppercase font-bold text-slate-600 my-3">
-        Client since: <span className="font-normal normal-case">fecha</span>
-      </p>
+            <p className="uppercase font-bold text-slate-600 my-3">
+              Client since:{" "}
+              <span className="font-normal normal-case">{client.date}</span>
+            </p>
 
-      <p className="uppercase font-bold text-slate-600 my-3">
-        Description:{" "}
-        <span className="font-normal normal-case">
-          loquitoloquitoloquitoloquito loquito
-        </span>
-      </p>
-    </div>
+            <p className="uppercase font-bold text-slate-600 my-3">
+              Description:{" "}
+              <span className="font-normal normal-case">
+                {client.description}
+              </span>
+            </p>
+          </div>
+        ))}
+    </>
   );
 };
 
--- src/components/ClientsList.tsx
+++ src/components/ClientsList.tsx
@@ -5,8 +5,6 @@ const ClientsList = () => {
     <div className="md:w-1/2 w-full md:h-screen md:overflow-y-scroll">
       <h2 className="font-bold text-2xl my-6 text-center">Clients list</h2>
       <Client />
-      <Client />
-      <Client />
     </div>
   );
 };
