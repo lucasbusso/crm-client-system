8ec34922bcbc987023a441b93e0a840dfaf03233
Lucas Busso <lucasxbusso@gmail.com>
2023-09-11 13:23:49 -0300

    client context update changes

--- src/context/client.context.tsx
+++ src/context/client.context.tsx
@@ -5,13 +5,14 @@ import React, {
   useContext,
   useState,
 } from "react";
-import { Client } from "../interfaces/form.interface";
+import { Client, ClientEdit } from "../interfaces/form.interface";
 
 type ContextProps = {
   clients: Client[];
   setClients: Dispatch<SetStateAction<Client[]>>;
-  isEditing: string;
-  setIsEditing: Dispatch<SetStateAction<string>>;
+  isEditing: string | undefined;
+  setIsEditing: Dispatch<SetStateAction<string | undefined>>;
+  updateClient: (client: ClientEdit) => void;
 };
 
 const ClientContext = createContext<ContextProps | null>(null);
@@ -20,12 +21,24 @@ export const ClientProvider: React.FC<{
   children: JSX.Element;
 }> = ({ children }) => {
   const [clients, setClients] = useState<Client[]>([]);
-  const [isEditing, setIsEditing] = useState<string>("");
+  const [isEditing, setIsEditing] = useState<string | undefined>("");
+
+  const updateClient = (updatedClient: ClientEdit) => {
+    setClients((prevClients) =>
+      prevClients.map((client) =>
+        client.id === updatedClient.id
+          ? { ...client, ...updatedClient }
+          : client
+      )
+    );
+  };
+
   const value: ContextProps = {
     clients,
     setClients,
     isEditing,
     setIsEditing,
+    updateClient,
   };
   return (
     <ClientContext.Provider value={value}>{children}</ClientContext.Provider>
