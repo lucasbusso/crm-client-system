32012f0e4d7baca55ed990520d47f365307caab5
Lucas Busso <lucasxbusso@gmail.com>
2023-09-14 16:10:48 -0300

    redux store provider wrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { Container } from "react-bootstrap";
 import { ClientProvider } from "./context/client.context";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./Router";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 function App() {
   return (
-    <ClientProvider>
-      <BrowserRouter>
-        <Container>
-          <AppRouter />
-        </Container>
-      </BrowserRouter>
-    </ClientProvider>
+    <Provider store={store}>
+      <ClientProvider>
+        <BrowserRouter>
+          <Container>
+            <AppRouter />
+          </Container>
+        </BrowserRouter>
+      </ClientProvider>
+    </Provider>
   );
 }
 
