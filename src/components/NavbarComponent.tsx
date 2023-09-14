42310f7d032f8f787dfcfb86cdbc469afa20863a
Lucas Busso <lucasxbusso@gmail.com>
2023-09-14 16:13:12 -0300

    navbar dynamic render based on auth state

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,22 +1,44 @@
-import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { NavigateFunction, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { logout } from "../redux/slices/auth.slice";
 
 const NavbarComponent = () => {
   const navigate: NavigateFunction = useNavigate();
+  const { isAuth } = useAppSelector((state) => state.authReducer);
+  const dispatch = useAppDispatch();
+
+  function handleLogout() {
+    dispatch(logout());
+  }
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container className="bg-indigo-500 h-[64px]">
-        <Navbar.Brand
-          href="#"
-          className="font-bold text-white text-center text-3xl w-full flex justify-between"
+      <Container className="bg-indigo-500 h-[64px] w-full flex justify-between">
+        <h1
+          className="font-bold text-white text-center text-3xl cursor-pointer"
+          onClick={() => navigate("/")}
         >
-          <h1 onClick={() => navigate("/")}>✦ Client Manager ✦</h1>
-          <Button variant="outline-light" onClick={() => navigate("login")}>
-            Login
+          ✦ Client Manager ✦
+        </h1>
+        {isAuth ? (
+          <Button
+            className="bg-white hover:bg-slate-300 text-lg font-semibold uppercase text-indigo-500 hover:text-indigo-500 border-none rounded-md p-2 leading-none"
+            onClick={() => handleLogout()}
+          >
+            Logout
           </Button>
-        </Navbar.Brand>
+        ) : (
+          <Link
+            to="login"
+            className="bg-white hover:bg-slate-300 text-lg font-semibold uppercase text-indigo-500 hover:text-indigo-500 border-none rounded-md p-2 leading-none"
+          >
+            Login
+          </Link>
+        )}
       </Container>
     </Navbar>
   );
