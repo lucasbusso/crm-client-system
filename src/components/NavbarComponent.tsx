6c7767e6086f2016d68ea959b75d1ffb13510a28
Lucas Busso <lucasxbusso@gmail.com>
2023-09-18 20:32:59 -0300

    navbar UI updates and auth cookie removal

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,18 +1,21 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import { NavigateFunction, useNavigate } from "react-router-dom";
+import { NavigateFunction, useNavigate, Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { useCookies } from "react-cookie";
+import { Button, NavDropdown } from "react-bootstrap";
 import { logout } from "../redux/slices/auth.slice";
 
 const NavbarComponent = () => {
   const navigate: NavigateFunction = useNavigate();
-  const { isAuth } = useAppSelector((state) => state.authReducer);
+  const { isAuth, userData } = useAppSelector((state) => state.authReducer);
   const dispatch = useAppDispatch();
+  const [, , remove] = useCookies();
 
   function handleLogout() {
     dispatch(logout());
+    remove("accessToken");
+    navigate("/login");
   }
 
   return (
@@ -24,20 +27,10 @@ const NavbarComponent = () => {
         >
           ✦ Client Manager ✦
         </h1>
-        {isAuth ? (
-          <Button
-            className="bg-white hover:bg-slate-300 text-lg font-semibold uppercase text-indigo-500 hover:text-indigo-500 border-none rounded-md p-2 leading-none"
-            onClick={() => handleLogout()}
-          >
-            Logout
-          </Button>
-        ) : (
-          <Link
-            to="login"
-            className="bg-white hover:bg-slate-300 text-lg font-semibold uppercase text-indigo-500 hover:text-indigo-500 border-none rounded-md p-2 leading-none"
-          >
-            Login
-          </Link>
+        {isAuth && (
+          <NavDropdown title={`Hi ${userData?.email}`} className="text-white">
+            <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+          </NavDropdown>
         )}
       </Container>
     </Navbar>
