fcecd7a452cba86b626d30731a1189a4cdcd914c
Lucas Busso <lucasxbusso@gmail.com>
2023-09-18 20:33:50 -0300

    login user state

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,35 +1,59 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { Button } from "react-bootstrap";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { login } from "../redux/slices/auth.slice";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
+import { authThunk } from "../redux/thunks/auth.thunk";
+
+type User = {
+  username: string;
+  password: string;
+};
 
 export const LoginPage: React.FC<{}> = () => {
   const { isAuth } = useAppSelector((state) => state.authReducer);
+  const navigate = useNavigate();
+  const [user, setUser] = useState<User>({ username: "", password: "" });
 
   const dispatch = useAppDispatch();
-  function handleLogin() {
-    dispatch(login());
+  function handleSubmit() {
+    console.log(user);
+    dispatch(authThunk(user));
+    navigate("/");
   }
+
+  function handleLogin(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  }
+
   return isAuth ? (
     <Navigate to="/" replace />
   ) : (
     <form className="container flex flex-column gap-4 w-[50%] mt-[64px]">
       <input
+        name="username"
         type="email"
         placeholder="Email"
         className="border-2 p-2 rounded-md"
+        value={user.username}
+        onChange={handleLogin}
       />
       <input
+        name="password"
         type="password"
         placeholder="Password"
         className="border-2 p-2 rounded-md"
+        value={user.password}
+        onChange={handleLogin}
       />
       <Button
         type="submit"
         variant="primary"
         className="bg-indigo-500 hover:bg-indigo-600 uppercase"
-        onClick={() => handleLogin()}
+        onClick={() => handleSubmit()}
       >
         Login
       </Button>
