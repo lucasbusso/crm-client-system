0367543dd9d9aab600e5c6dcd09f12368568452e
Lucas Busso <lucasxbusso@gmail.com>
2023-09-18 20:34:15 -0300

    login slice reducer update

--- src/redux/slices/auth.slice.ts
+++ src/redux/slices/auth.slice.ts
@@ -1,7 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { authThunk } from "../thunks/auth.thunk";
+import { RejectedActionFromAsyncThunk } from "@reduxjs/toolkit/dist/matchers";
+import { expirationTokenAuth, getCookie, tokenDecode } from "../../utils";
+import { TokenFirebase } from "../../interfaces/firebase.interface";
 
-const initialState: { isAuth: boolean } = {
-  isAuth: false,
+interface AuthState {
+  isAuth: boolean;
+  success: boolean;
+  error: RejectedActionFromAsyncThunk<any> | null;
+  loading: boolean;
+  userData: {
+    email: string | null;
+    uid: string | null;
+  } | null;
+  accessToken: string | null | undefined;
+  isExpired: boolean | null;
+}
+
+const initialState: AuthState = {
+  isAuth:
+    getCookie("accessToken") !== undefined
+      ? !expirationTokenAuth(getCookie("accessToken")!)
+      : false,
+  accessToken:
+    getCookie("accessToken") !== undefined ? getCookie("accessToken") : null,
+  error: null,
+  isExpired:
+    getCookie("accessToken") !== undefined
+      ? expirationTokenAuth(getCookie("accessToken")!)
+      : null,
+  loading: false,
+  success: getCookie("accessToken") !== undefined,
+  userData:
+    getCookie("accessToken") !== undefined
+      ? {
+          email: tokenDecode<TokenFirebase>(getCookie("accessToken")!).email,
+          uid: tokenDecode<TokenFirebase>(getCookie("accessToken")!).sub,
+        }
+      : null,
 };
 
 export const authSlice = createSlice({
@@ -14,6 +50,25 @@ export const authSlice = createSlice({
     logout: (state) => {
       state.isAuth = false;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(authThunk.pending, (state) => {
+      return (state = { ...initialState, loading: true });
+    });
+    builder.addCase(authThunk.rejected, (state, action) => {
+      return (state = { ...initialState, loading: false, error: action.error });
+    });
+    builder.addCase(authThunk.fulfilled, (state, action) => {
+      return (state = {
+        ...initialState,
+        loading: false,
+        success: true,
+        accessToken: action.payload.accessToken,
+        isAuth: true,
+        isExpired: false,
+        userData: action.payload.userData,
+      });
+    });
   },
 });
 
