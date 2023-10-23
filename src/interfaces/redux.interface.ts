641c6514645a282ed808ea239a32e7c2528f5a3c
Lucas Busso <lucasxbusso@gmail.com>
2023-10-23 16:43:31 -0300

    redux user interfaces

---
+++ src/interfaces/redux.interface.ts
@@ -0,0 +1,11 @@
+export interface AuthCredentials {
+  email: string;
+  password: string;
+}
+
+export interface RegistrationData {
+  email: string;
+  password: string;
+  name: string;
+  age: number;
+}
