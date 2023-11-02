af5b81b93b74391437bd1f47918c5ca9a4578fee
Lucas Busso <lucasxbusso@gmail.com>
2023-11-02 14:54:12 -0300

    old localStorage modules deleted

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,4 @@ export { default as RoutesNotFound } from "./RoutesNotFound";
 export * from "./decodeToken";
 export * from "./formatDate";
 export * from "./getCookie";
-export * from "./localStorage";
 export * from "./validateForm";
