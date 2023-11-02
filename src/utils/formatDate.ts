cd8cf80a5530751f0e993ef6efe264379430f6b9
Lucas Busso <lucasxbusso@gmail.com>
2023-11-02 14:58:41 -0300

    date format change

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,7 @@
 import moment from "moment";
 
 const formatDate = (date: string) => {
-  const dateFormated = moment(date).format("YYYY-MM-DD HH:mm:ss");
+  const dateFormated = moment(date).format("DD-MM-YYYY HH:mm");
   return dateFormated;
 };
 
